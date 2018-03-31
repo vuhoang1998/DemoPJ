94fc8e0181d8ccd210cd74399ffd83c0077e3359
DESKTOP-O5UFE3V\hoang <hoangnv1412@gmail.com>
2018-03-31 20:57:00 +0700

    2activity

--- app/src/main/java/com/example/hoang/demopj/RegisterActivity.java
+++ app/src/main/java/com/example/hoang/demopj/RegisterActivity.java
@@ -3,19 +3,15 @@ package com.example.hoang.demopj;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //asdasd
+        setContentView(R.layout.activity_register);
     }
-
     @Override
     protected void onStart() {
         super.onStart();
     }
-
-
 }
