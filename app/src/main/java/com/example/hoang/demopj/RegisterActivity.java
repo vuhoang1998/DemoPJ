d9d38d37a6342e8b7e3f505e418451d1f12412db
DESKTOP-O5UFE3V\hoang <hoangnv1412@gmail.com>
2018-04-04 13:38:32 +0700

    4/4/2018

--- app/src/main/java/com/example/hoang/demopj/RegisterActivity.java
+++ app/src/main/java/com/example/hoang/demopj/RegisterActivity.java
@@ -1,17 +1,35 @@
 package com.example.hoang.demopj;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    @BindView(R.id.bt_next)
+    Button btnext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
     }
+
     @Override
     protected void onStart() {
+
         super.onStart();
+    }
+
+    @OnClick(R.id.bt_next)
+    public void onViewClicked() {
+        Intent intent = new Intent(RegisterActivity.this,PlayActivity.class);
+        startActivity(intent);
     }
 }
