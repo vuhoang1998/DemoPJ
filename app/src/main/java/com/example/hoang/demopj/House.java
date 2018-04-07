017671bc1dc01f586b5b076c0be671750acd7936
vudongbui <vudongbui@gmail.com>
2018-04-07 23:58:51 +0700

    String name

--- app/src/main/java/com/example/hoang/demopj/House.java
+++ app/src/main/java/com/example/hoang/demopj/House.java
@@ -15,7 +15,7 @@ public class House {
 
     private Random random = new Random();
 
-    public House(int posHouse, int price, int playerOccupy, int lvHouse, int houseId) {
+    public House(String name,int posHouse, int price, int playerOccupy, int lvHouse, int houseId) {
         this.posHouse = posHouse;
         this.lvHouse = lvHouse;
         this.price = price;
@@ -24,21 +24,21 @@ public class House {
     }
 
     public static House[] houses = new House[] {
-      new House(1, 2000, 0, 0, R.id.tv_house_01),
-      new House(2, 2000, 0, 0, R.id.tv_house_02),
-      new House(3, 2000, 0, 0, R.id.tv_house_03),
-      new House(4, 2000, 0, 0, R.id.tv_house_04),
-      new House(5, 2000, 0, 0, R.id.tv_house_05),
-      new House(6, 2000, 0, 0, R.id.tv_house_06),
-      new House(7, 2000, 0, 0, R.id.tv_house_07),
-      new House(8, 2000, 0, 0, R.id.tv_house_08),
-      new House(9, 2000, 0, 0, R.id.tv_house_09),
-      new House(10, 2000, 0, 0, R.id.tv_house_10),
-      new House(11, 2000, 0, 0, R.id.tv_house_11),
-      new House(12, 2000, 0, 0, R.id.tv_house_12),
-      new House(13, 2000, 0, 0, R.id.tv_house_13),
-      new House(14, 2000, 0, 0, R.id.tv_house_14),
-      new House(15, 2000, 0, 0, R.id.tv_house_15),
-      new House(16, 2000, 0, 0, R.id.tv_house_16)
+            new House("Casterly Rock",1, 2000, 0, 0, R.id.tv_house_01),
+            new House("Winterfell",2, 2000, 0, 0, R.id.tv_house_02),
+            new House("Eyrie",3, 2000, 0, 0, R.id.tv_house_03),
+            new House("Highgarden",4, 2000, 0, 0, R.id.tv_house_04),
+            new House("Harrenhal",5, 2000, 0, 0, R.id.tv_house_05),
+            new House("Qarth",6, 2000, 0, 0, R.id.tv_house_06),
+            new House("Braavos",7, 2000, 0, 0, R.id.tv_house_07),
+            new House("Dragon Stone",8, 2000, 0, 0, R.id.tv_house_08),
+            new House("Oldtown",9, 2000, 0, 0, R.id.tv_house_09),
+            new House("Moat Cailin",10, 2000, 0, 0, R.id.tv_house_10),
+            new House("Valryia",11, 2000, 0, 0, R.id.tv_house_11),
+            new House("Bloody Gate",12, 2000, 0, 0, R.id.tv_house_12),
+            new House("Storm End",13, 2000, 0, 0, R.id.tv_house_13),
+            new House("King Landing",14, 2000, 0, 0, R.id.tv_house_14),
+            new House("Riverrun",15, 2000, 0, 0, R.id.tv_house_15),
+            new House("Sunspear",16, 2000, 0, 0, R.id.tv_house_16)
     };
 }
