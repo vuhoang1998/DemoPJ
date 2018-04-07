02f4527e2f5d747555fc9219dae1222a4afe05c9
YukiShiori <anmana000@gmail.com>
2018-04-07 23:05:49 +0700

    Added house array

--- app/src/main/java/com/example/hoang/demopj/House.java
+++ app/src/main/java/com/example/hoang/demopj/House.java
@@ -1,5 +1,7 @@
 package com.example.hoang.demopj;
 
+import java.util.Random;
+
 /**
  * Created by Owner on 4/7/2018.
  */
@@ -10,6 +12,8 @@ public class House {
     int playerOccupy;
     int lvHouse;
 
+    private Random random = new Random();
+
     public House(int posHouse, int price, int playerOccupy, int lvHouse, int houseId) {
         this.posHouse = posHouse;
         this.lvHouse = lvHouse;
@@ -19,21 +23,20 @@ public class House {
 
     public static House[] houses = new House[] {
       new House(1, 2000, 0, 0, R.id.tv_house_01),
-      new House(1, 2000, 0, 0, R.id.tv_house_02),
-      new House(1, 2000, 0, 0, R.id.tv_house_03),
-      new House(1, 2000, 0, 0, R.id.tv_house_04),
-      new House(1, 2000, 0, 0, R.id.tv_house_05),
-      new House(1, 2000, 0, 0, R.id.tv_house_06),
-      new House(1, 2000, 0, 0, R.id.tv_house_07),
-      new House(1, 2000, 0, 0, R.id.tv_house_08),
-      new House(1, 2000, 0, 0, R.id.tv_house_09),
-      new House(1, 2000, 0, 0, R.id.tv_house_10),
-      new House(1, 2000, 0, 0, R.id.tv_house_11),
-      new House(1, 2000, 0, 0, R.id.tv_house_12),
-      new House(1, 2000, 0, 0, R.id.tv_house_13),
-      new House(1, 2000, 0, 0),
-      new House(1, 2000, 0, 0),
-      new House(1, 2000, 0, 0),
-      new House(1, 2000, 0, 0)
+      new House(2, 2000, 0, 0, R.id.tv_house_02),
+      new House(3, 2000, 0, 0, R.id.tv_house_03),
+      new House(4, 2000, 0, 0, R.id.tv_house_04),
+      new House(5, 2000, 0, 0, R.id.tv_house_05),
+      new House(6, 2000, 0, 0, R.id.tv_house_06),
+      new House(7, 2000, 0, 0, R.id.tv_house_07),
+      new House(8, 2000, 0, 0, R.id.tv_house_08),
+      new House(9, 2000, 0, 0, R.id.tv_house_09),
+      new House(10, 2000, 0, 0, R.id.tv_house_10),
+      new House(11, 2000, 0, 0, R.id.tv_house_11),
+      new House(12, 2000, 0, 0, R.id.tv_house_12),
+      new House(13, 2000, 0, 0, R.id.tv_house_13),
+      new House(14, 2000, 0, 0, R.id.tv_house_14),
+      new House(15, 2000, 0, 0, R.id.tv_house_15),
+      new House(16, 2000, 0, 0, R.id.tv_house_16)
     };
 }
